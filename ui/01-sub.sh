cf7579a0d18ee52a086dfc6fd502c391c482d14e
tiehfood <hummus-plan.0j@icloud.com>
2025-02-04 22:36:03 +0100

    feat: Add option to use single column all the time (#30)

--- ui/01-sub.sh
+++ ui/01-sub.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env ash
 
-if [ ! -z "$LOCAL_API_PORT" ]; then
-  export LOCAL_API_URL="http://localhost:${LOCAL_API_PORT}/"
-fi
+CONFIG_FILE=/etc/nginx/conf.d/01-sub_filter.conf
 
-cat << EOF > /etc/nginx/conf.d/01-sub_filter.conf
+cat << EOF > "$CONFIG_FILE"
 sub_filter_types application/javascript;
-sub_filter 'currency:"EUR"' 'currency:"${CURRENCY_ISO:-EUR}"';
-sub_filter 'fetch("api/' 'fetch("${LOCAL_API_URL}api/';
 sub_filter_once off;
 EOF
+
+if [ -n "$CURRENCY_ISO" ]; then
+  cat << EOF >> "$CONFIG_FILE"
+sub_filter 'currency:"EUR"' 'currency:"${CURRENCY_ISO}"';
+EOF
+fi
+
+if [ -n "$LOCAL_API_PORT" ]; then
+  cat << EOF >> "$CONFIG_FILE"
+sub_filter 'fetch("api/' 'fetch("http://localhost:${LOCAL_API_PORT}/api/';
+EOF
+fi
+
+if [ "$USE_SINGLE_COLUMN" = true ]; then
+  cat << EOF >> "$CONFIG_FILE"
+sub_filter 'md:grid-cols-2 ' '';
+EOF
+fi
